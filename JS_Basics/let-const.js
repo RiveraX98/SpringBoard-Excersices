418707f7e2118dc23972bd1a3f273077fe66d260
Xavier Rivera <xrivera@Xaviers-MacBook-Air.local>
2023-04-25 20:06:15 -0400

    let-const.js added

---
+++ JS_Basics/let-const.js
@@ -0,0 +1,16 @@
+//var PI = 3.14;
+//PI = 42;  stop me from doing this!
+const PI = 3.14
+
+
+
+
+//- What is the difference between var and let?
+You can reassign and redeclare with var, but you can not redeclare using the let
+Let has a block scope.
+//- What is the difference between var and const?
+You can reassign and redeclare with var, but not with const and const has a block scope
+//- What is the difference between let and const ?
+You can reassign with let, but you cant with const 
+// - What is hoisting?
+Variables are put to the top of the scope they are declared in
